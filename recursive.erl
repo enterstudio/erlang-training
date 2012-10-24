58c57efc76329f4c28a17485c02caabd94827238
Cameron Dykes <yellow5c@gmail.com>
2012-10-23 23:49:58 -0500

    Add recursive:lenient_zip

--- recursive.erl
+++ recursive.erl
@@ -1,5 +1,5 @@
 -module(recursive).
--export([fac/1, len/1, tail_fac/1, tail_len/1, duplicate/2, tail_duplicate/2, reverse/1, tail_reverse/1, sublist/2, tail_sublist/2, zip/2]).
+-export([fac/1, len/1, tail_fac/1, tail_len/1, duplicate/2, tail_duplicate/2, reverse/1, tail_reverse/1, sublist/2, tail_sublist/2, zip/2, lenient_zip/2]).
 
 fac(0) -> 1;
 fac(N) when N > 0 -> N*fac(N-1).
@@ -51,3 +51,8 @@ tail_sublist([H|T], N, SubList) when N > 0 ->
 zip([], []) -> [];
 zip([X|Xs], [Y|Ys]) ->
   [{X, Y}|zip(Xs, Ys)].
+
+lenient_zip([], _) -> [];
+lenient_zip(_, []) -> [];
+lenient_zip([X|Xs], [Y|Ys]) ->
+  [{X, Y}|lenient_zip(Xs, Ys)].
