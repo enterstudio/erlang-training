3c241324fc9989ec0896b53cbf07d0a3c36217a4
Cameron Dykes <yellow5c@gmail.com>
2012-10-24 07:53:31 -0500

    Cleanup recursive export code

--- recursive.erl
+++ recursive.erl
@@ -1,5 +1,10 @@
 -module(recursive).
--export([fac/1, len/1, tail_fac/1, tail_len/1, duplicate/2, tail_duplicate/2, reverse/1, tail_reverse/1, sublist/2, tail_sublist/2, zip/2, lenient_zip/2]).
+-export(
+  [
+    fac/1, len/1, tail_fac/1, tail_len/1, duplicate/2, tail_duplicate/2,
+    reverse/1, tail_reverse/1, sublist/2, tail_sublist/2, zip/2, lenient_zip/2
+  ]
+).
 
 fac(0) -> 1;
 fac(N) when N > 0 -> N*fac(N-1).
