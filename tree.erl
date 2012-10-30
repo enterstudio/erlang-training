b3dd6ca3bc7c03fe63b7593cda5a814e434a7532
Cameron Dykes <yellow5c@gmail.com>
2012-10-29 23:28:01 -0500

    Add tree:insert

--- tree.erl
+++ tree.erl
@@ -1,4 +1,13 @@
 -module(tree).
--export([empty/0]).
+-export([empty/0, insert/3]).
 
 empty() -> {node, 'nil'}.
+
+insert(Key, Val, {node, 'nil'}) ->
+  {node, {Key, Val, {node, 'nil'}, {node, 'nil'}}};
+insert(NewKey, NewVal, {node, {Key, Val, Smaller, Larger}}) when NewKey < Key ->
+  {node, {Key, Val, insert(NewKey, NewVal, Smaller), Larger}};
+insert(NewKey, NewVal, {node, {Key, Val, Smaller, Larger}}) when NewKey > Key ->
+  {node, {Key, Val, Smaller, insert(NewKey, NewVal, Larger)}};
+insert(Key, Val, {node, {Key, _, Smaller, Larger}}) ->
+  {node, {Key, Val, Smaller, Larger}}.
