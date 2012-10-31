28f713582c01c363e00075e18009e2f0e25ea9c7
Cameron Dykes <yellow5c@gmail.com>
2012-10-30 23:07:53 -0500

    Add hhfuns:increment and hhfuns:decrement

--- hhfuns.erl
+++ hhfuns.erl
@@ -5,3 +5,9 @@ one() -> 1.
 two() -> 2.
 
 add(X,Y) -> X() + Y().
+
+increment([])    -> [];
+increment([H|T]) -> [H+1|increment(T)].
+
+decrement([])    -> [];
+decrement([H|T]) -> [H-1|decrement(T)].
