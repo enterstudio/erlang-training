b71d7f6962edefbb1468060dafd44c0b8021cae2
Cameron Dykes <yellow5c@gmail.com>
2012-10-30 23:13:43 -0500

    Add hhfuns:map, incr, and decr

--- hhfuns.erl
+++ hhfuns.erl
@@ -11,3 +11,9 @@ increment([H|T]) -> [H+1|increment(T)].
 
 decrement([])    -> [];
 decrement([H|T]) -> [H-1|decrement(T)].
+
+map(_, [])    -> [];
+map(F, [H|T]) -> [F(H)|map(F,T)].
+
+incr(X) -> X + 1.
+decr(X) -> X - 1.
