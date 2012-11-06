521664dd45c67113659d8596230680fc4c2fde31
Cameron Dykes <yellow5c@gmail.com>
2012-11-05 23:03:55 -0600

    Add exceptions:errors and exceptions:exits

--- exceptions.erl
+++ exceptions.erl
@@ -7,3 +7,17 @@ throws(F) ->
   catch
     Throw -> {throw, caught, Throw}
   end.
+
+errors(F) ->
+  try F() of
+    _ -> ok
+  catch
+    error:Error -> {error, caught, Error}
+  end.
+
+exits(F) ->
+  try F() of
+    _ -> ok
+  catch
+    exit:Exit -> {exit, caught, Exit}
+  end.
