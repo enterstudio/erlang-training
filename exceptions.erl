e85e7906065d3dbe9279e2e941d793911d3de21f
Cameron Dykes <yellow5c@gmail.com>
2012-11-05 23:15:19 -0600

    Add exceptions:sword/1, exceptions:black_knight/1, and exceptions:talk/0

--- exceptions.erl
+++ exceptions.erl
@@ -21,3 +21,21 @@ exits(F) ->
   catch
     exit:Exit -> {exit, caught, Exit}
   end.
+
+sword(1) -> throw(slice);
+sword(2) -> erlang:error(cut_arm);
+sword(3) -> exit(cut_leg);
+sword(4) -> throw(punch);
+sword(5) -> exit(cross_bridge).
+
+black_knight(Attack) when is_function(Attack, 0) ->
+  try Attack() of
+    _ -> "None shall pass."
+  catch
+    throw:slice   -> "It is but a scratch.";
+    error:cut_arm -> "I've had worse.";
+    exit:cut_leg  -> "Come on you pansy!";
+    _:_           -> "Just a flesh wound."
+  end.
+
+talk() -> "blah blah".
