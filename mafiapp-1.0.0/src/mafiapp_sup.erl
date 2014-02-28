f5f288c149e6c2558cf76d4d21824c7403f1f22a
Cameron Dykes <yellow5c@gmail.com>
2014-02-27 22:41:10 -0600

    Add mafiapp_sup module

---
+++ mafiapp-1.0.0/src/mafiapp_sup.erl
@@ -0,0 +1,13 @@
+-module(mafiapp_sup).
+-behavior(supervisor).
+
+-export([start_link/0]).
+-export([init/1]).
+
+start_link() ->
+  supervisor:start_link(?MODULE, []).
+
+%% This does absolutely nothing, only there to
+%% allow to wait for tables.
+init([]) ->
+  {ok, {{one_for_one, 1, 1}, []}}.
