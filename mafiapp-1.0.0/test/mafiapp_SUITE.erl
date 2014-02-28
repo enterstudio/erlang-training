c60b69325c9ebb8c8d3487882136ff13d7719177
Cameron Dykes <yellow5c@gmail.com>
2014-02-27 23:32:53 -0600

    Add test verifying mafiapp:add_service/4 functionality

--- mafiapp-1.0.0/test/mafiapp_SUITE.erl
+++ mafiapp-1.0.0/test/mafiapp_SUITE.erl
@@ -1,9 +1,12 @@
 -module(mafiapp_SUITE).
 -include_lib("common_test/include/ct.hrl").
 
--export([init_per_suite/1, end_per_suite/1, all/0]).
+-export([init_per_suite/1, end_per_suite/1,
+         init_per_test_case/2, end_per_test_case/2,
+         all/0]).
+-export([add_service/1]).
 
-all() -> [].
+all() -> [add_service].
 
 init_per_suite(Config) ->
   Priv = ?config(priv_dir, Config),
@@ -16,3 +19,27 @@ init_per_suite(Config) ->
 end_per_suite(_Config) ->
   application:stop(mnesia),
   ok.
+
+init_per_test_case(add_service, Config) ->
+  Config.
+
+end_per_test_case(_, _Config) ->
+  ok.
+
+%% Services can go both way: from a friend to the boss, or
+%% from the boss to a friend! A boss friend is required!
+add_service(_Config) ->
+  {error, unknown_friend} = mafiapp:add_service("from name",
+                                                "to name",
+                                                {1946, 5, 23},
+                                                "a fake service"),
+  ok = mafiapp:add_friend("Don Corleone", [], [boss], boss),
+  ok = mafiapp:add_friend("Alan Parsons",
+                          [{twitter, "@ArtScienceSound"}],
+                          [{born, {1948, 12, 20}},
+                           musician, 'audio engineer',
+                           producer, "has projects"],
+                          mixing),
+  ok = mafiapp:add_service("Alan Parsons", "Don Corleone",
+                           {1973, 3, 1},
+                           "Helped release a Pink Floyd album").
