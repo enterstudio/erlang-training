ec3fdaae934760cb07ef5ff6a98b0eb216ec5a0c
Cameron Dykes <yellow5c@gmail.com>
2014-03-02 23:47:50 -0600

    Add convert module with main/0

---
+++ misc/convert.erl
@@ -0,0 +1,11 @@
+-module(convert).
+-export([main/0]).
+
+main() ->
+  [_,_] = convert({a,b}),
+  {_,_} = convert([a,b]),
+  [_,_] = convert([a,b]),
+  {_,_} = convert({a,b}).
+
+convert(Tup) when is_tuple(Tup) -> tuple_to_list(Tup);
+convert(L = [_|_]) -> list_to_tuple(L).
